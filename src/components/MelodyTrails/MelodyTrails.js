314b1a9ba89f65ede8cfa10115a12d0edcaadbe0
Nitzan Cohen <nitzancohen@hotmail.com>
2023-01-03 01:43:08 +0200

    Little change 3

--- src/components/MelodyTrails/MelodyTrails.js
+++ src/components/MelodyTrails/MelodyTrails.js
@@ -6,7 +6,8 @@ function MelodyTrails(props) {
         <section className="melody-trails">
             <div>
                 Melody Trails - Hackutism.
-                Developer: Nitzan Cohen
+                Developer: Nitzan Cohen.
+                Here is the view of melodies.
             </div>
         </section>
     )
