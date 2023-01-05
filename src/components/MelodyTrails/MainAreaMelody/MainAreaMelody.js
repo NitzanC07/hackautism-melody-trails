4f0869fbf7ce982fbc76adb41b6df7852fd8fc7a
Nitzan Cohen <nitzancohen@hotmail.com>
2023-01-06 01:24:51 +0200

    Created colors musical notes, and made an example style, not formal style

--- src/components/MelodyTrails/MainAreaMelody/MainAreaMelody.js
+++ src/components/MelodyTrails/MainAreaMelody/MainAreaMelody.js
@@ -4,6 +4,22 @@ function MainAreaMelody(props) {
 
     return (
         <section className="main-area-melody">
+            <div className='row__7_high'></div>
+            <div className='row__7_low'></div>
+            <div className='row__6_high'></div>
+            <div className='row__6_low'></div>
+            <div className='row__5_high'></div>
+            <div className='row__5_low'></div>
+            <div className='row__5_high'></div>
+            <div className='row__5_low'></div>
+            <div className='row__4_high'></div>
+            <div className='row__4_low'></div>
+            <div className='row__3_high'></div>
+            <div className='row__3_low'></div>
+            <div className='row__2_high'></div>
+            <div className='row__2_low'></div>
+            <div className='row__1_high'></div>
+            <div className='row__1_low'></div>
             <div className='note__1__high'></div>
             <div className='note__2__high'></div>
             <div className='note__3__high'></div>
