002c96befaf81a9d22dca424582486cbcdc6c262
Nitzan Cohen <nitzancohen@hotmail.com>
2023-01-06 18:51:05 +0200

    Added sound to the white notes, just one sound for every keys

--- src/components/PianoKeyboard/__Octave/PianoNote/PianoNoteWhite.js
+++ src/components/PianoKeyboard/__Octave/PianoNote/PianoNoteWhite.js
@@ -1,12 +1,35 @@
 import './_white/piano-note_white.css';
 import './piano-note.css';
+import { useEffect, useRef } from 'react';
 
 function PianoNoteWhite(props) {
+
+    const audioCtx = useRef(new AudioContext());
+    const oscillator = useRef(audioCtx.current.createOscillator());
+
+    useEffect(() => {
+        oscillator.current.type = 'sine';
+        oscillator.current.frequency.value = 440;
+        oscillator.current.connect(audioCtx.current.destination);
+
+        // return () => {
+        //     oscillator.current.stop();
+        // }
+    }, []);
+
+    function handlePlayNote() {
+        console.log(`Play ${props.pos} ${props.note}\n${audioCtx}`);
+        oscillator.current.start();
+        setTimeout(() => {
+            oscillator.current.stop();
+        }, 1000)
+    }
 
     return (
         <button 
             className='piano-note piano-note_white' 
             id={props.pos}
+            onClick={handlePlayNote}
         >
             {props.pos} {props.note}
         </button>
