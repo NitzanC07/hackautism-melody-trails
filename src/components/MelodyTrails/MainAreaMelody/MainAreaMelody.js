7aa0e3907f3a68b19310e6ec0da3ab2ce73506b6
Nitzan Cohen <nitzancohen@hotmail.com>
2023-01-13 03:25:05 +0200

    New UI

--- src/components/MelodyTrails/MainAreaMelody/MainAreaMelody.js
+++ src/components/MelodyTrails/MainAreaMelody/MainAreaMelody.js
@@ -1,43 +1,21 @@
 import './main-area-melody.css';
+import './__bar/main-area-melody__bar.css';
 
 function MainAreaMelody(props) {
 
     return (
         <section className="main-area-melody">
-            <div className='row__7_high'></div>
+            <div className="main-area-melody__bar"></div>
+            
+
+            {/* <div className='row__7_high'></div>
             <div className='row__7_low'></div>
-            <div className='row__6_high'></div>
-            <div className='row__6_low'></div>
-            <div className='row__5_high'></div>
-            <div className='row__5_low'></div>
-            <div className='row__5_high'></div>
-            <div className='row__5_low'></div>
-            <div className='row__4_high'></div>
-            <div className='row__4_low'></div>
-            <div className='row__3_high'></div>
-            <div className='row__3_low'></div>
-            <div className='row__2_high'></div>
-            <div className='row__2_low'></div>
-            <div className='row__1_high'></div>
-            <div className='row__1_low'></div>
             <div className='main-area-melody__home'></div>
             <div className='note__1__high'></div>
-            <div className='note__2__high'></div>
-            <div className='note__3__high'></div>
-            <div className='note__4__high'></div>
-            <div className='note__5__high'></div>
-            <div className='note__6__high'></div>
-            <div className='note__7__high'></div>
-            <div className='note__7__low'></div>
-            <div className='note__6__low'></div>
-            <div className='note__5__low'></div>
-            <div className='note__4__low'></div>
-            <div className='note__3__low'></div>
-            <div className='note__2__low'></div>
-            <div className='note__1__low'></div>
-
+            <div className='note__1__low'></div> */}
         </section>
     )
 }
 
 export default MainAreaMelody;
+
--- src/components/MelodyTrails/MelodyTrails.js
+++ src/components/MelodyTrails/MelodyTrails.js
@@ -1,10 +1,26 @@
 import BottomAreaMelody from './BottomAreaMelody/BottomAreaMelody';
 import MainAreaMelody from './MainAreaMelody/MainAreaMelody';
 import TopAreaMelody from './TopAreaMelody/TopAreaMelody';
+import logoMelodyTrails from '../../images/logoMelodyTrails.png';
+import iconMenuPlay from '../../images/logos/menu-play.svg';
+import iconMenuLyrics from '../../images/logos/menu-lyrics.svg';
+import iconMenuMusicNote from '../../images/logos/menu-musicNote.svg';
+import iconMenuRecord from '../../images/logos/menu-record.svg';
 import './melody-trails.css';
-
+import './__menu/melody-trails__menu.css';
+import './__logo/melody-trails__logo.css';
+import './__menu-icons/melody-trails__menu-icons.css';
+import './__menu-icon/melody-trails__menu-icon.css';
+import { useState } from 'react';
 
 function MelodyTrails(props) {
+
+    const [isMenuOpen, setMenuOpen] = useState(false);
+
+    function handleMenuButton() {
+        console.log('Button menu was clicked.');
+        setMenuOpen(!isMenuOpen);
+    }
 
     return (
         <section className="melody-trails">
@@ -14,6 +30,48 @@ function MelodyTrails(props) {
             <MainAreaMelody /> 
 
             <BottomAreaMelody />
+
+            <div className='melody-trails__menu-icons'>
+                <button className="melody-trails__menu" onClick={handleMenuButton}>
+                    <img className="melody-trails__logo" src={logoMelodyTrails} alt="Melody Trails's logo" />
+                </button>
+                {
+                    isMenuOpen ?
+                    <div>
+                        <button 
+                            className='melody-trails__menu-icon'
+                            style={{'position': 'absolute', 'top': '24px', 'left': '24px'}}
+                        >
+                            פלייבק
+                            <img src={iconMenuMusicNote} alt='Listen to palyback' />
+                        </button>
+                        <button 
+                            className='melody-trails__menu-icon'
+                            style={{'position': 'absolute', 'top': '48px', 'left': '148px'}}
+                        >
+                            ליווי
+                            <img src={iconMenuPlay} alt='Play with soundtrack' />
+                        </button>
+                        <button 
+                            className='melody-trails__menu-icon'
+                            style={{'position': 'absolute', 'bottom': '24px', 'right': '24px'}}
+                        >
+                            הקלטה
+                            <img src={iconMenuRecord} alt='Record playing' />
+                        </button>
+                        <button 
+                            className='melody-trails__menu-icon'
+                            style={{'position': 'absolute', 'bottom': '148px', 'right': '48px'}}
+                        >
+                            מילים
+                            <img src={iconMenuLyrics} alt='Play with lyrics' />
+                        </button>
+                    </div>
+                     
+                :
+                ""
+                }
+            </div>
 
         </section>
     )
